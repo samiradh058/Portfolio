b7c7653353c38b55c50e60a1a0aa3a462e9320be
samiradh058 <adhikarisamir68@gmail.com>
2024-12-04 20:06:00 +0545

    Gap between mottos

--- components/mottos.js
+++ components/mottos.js
@@ -12,9 +12,9 @@ export default function Mottos() {
   const currentColors = colors[mode][currentTheme];
 
   return (
-    <div className="flex md:justify-between justify-around flex-row md:flex-col w-full md:col-span-5 lg:col-span-4 p-4">
+    <div className="flex md:justify-between justify-around flex-row md:flex-col w-full md:col-span-5 lg:col-span-4 p-4 gap-2">
       <div
-        className="self-start rounded-full sm:w-44 sm:h-44 w-40 h-40 flex items-center p-4 text-center text-[20px] sm:text-[24px] font-semibold animate-custom-bounce"
+        className="self-start rounded-full sm:w-44 sm:h-44 w-40 h-40 flex items-center p-4 text-center xs:text-[18px] text-[20px] sm:text-[24px] font-semibold animate-custom-bounce"
         style={{
           border: "4px solid",
           borderColor: currentColors.accent,
